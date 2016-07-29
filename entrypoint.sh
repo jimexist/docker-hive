456918a267786d5c861ff4933f4de7d64c3a6712
Jiayu Liu <jimexist@gmail.com>
2016-07-29 19:08:13 +0800

    mkdir for warehouse

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ rm /tmp/*.pid
 # start hdfs daemon and yarn daemon
 $HADOOP_PREFIX/sbin/start-dfs.sh
 $HADOOP_PREFIX/sbin/start-yarn.sh
+$HADOOP_PREFIX/bin/hadoop fs -mkdir -p /user/hive/warehouse
 # start hive metastore
 ./bin/hive --service metastore &
 # start hiveserver
