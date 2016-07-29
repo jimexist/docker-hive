4ffa8776c5566ef7dc5f0d8cb91c4c53ed3edd60
Jiayu Liu <jimexist@gmail.com>
2016-07-29 23:07:59 +0800

    update hive

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,10 @@
-#!/bin/sh
-: ${HADOOP_PREFIX:=/usr/local/hadoop}
-$HADOOP_PREFIX/etc/hadoop/hadoop-env.sh
-rm /tmp/*.pid
-# start hdfs daemon and yarn daemon
-$HADOOP_PREFIX/sbin/start-dfs.sh
-$HADOOP_PREFIX/sbin/start-yarn.sh
+#!/bin/bash
+/bin/bash /etc/bootstrap.sh
 $HADOOP_PREFIX/bin/hadoop fs -mkdir -p /user/hive/warehouse
+
 # start hive metastore
+echo "starting metastore"
 ./bin/hive --service metastore &
 # start hiveserver
+echo "starting hive server2"
 ./bin/hiveserver2
